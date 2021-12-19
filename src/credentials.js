aa49265ed07c2f5d24299a0016eabfc53ad9fc6f
Nguyễn Vinh Tiếng <79965595+vinhtieng@users.noreply.github.com>
2021-12-19 09:49:07 +0700

    Update credentials.js
    add url server

--- src/credentials.js
+++ src/credentials.js
@@ -1,3 +1,3 @@
-export const SERVER_URL = 'http://localhost:4000/api'
+export const SERVER_URL = 'https://server-vt-social-media.herokuapp.com/api'
 export const URL = 'http://localhost:3000'
 export const TOKEN_NAME = 'TOKEN_USER_SOCIAL_MEDIA_VINH_TIENG'
