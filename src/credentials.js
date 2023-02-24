7ecd18b2bf04c30ee4abb4c4cfed3bedb495ac33
NVT <79965595+nguyenvinhtieng@users.noreply.github.com>
2023-02-24 08:49:45 +0700

    Update credentials.js

--- src/credentials.js
+++ src/credentials.js
@@ -1,4 +1,4 @@
-export const SERVER_URL = 'https://server-vt-social-media.herokuapp.com/api'
-export const HOST = 'https://server-vt-social-media.herokuapp.com'
+export const SERVER_URL = 'https://server-vt-socialmedia.herokuapp.com/api'
+export const HOST = 'https://server-vt-socialmedia.herokuapp.com'
 export const URL = 'https://vinhtieng-social-media.netlify.app/'
 export const TOKEN_NAME = 'TOKEN_USER_SOCIAL_MEDIA_VINH_TIENG'
